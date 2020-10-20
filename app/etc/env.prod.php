a55c720805418d53fa608ed91f1e85b577f40d1b
Rostyslav Panteleichuk <r.panteleychuk@gmail.com>
2020-10-20 21:26:14 +0300

    #3 edit docker-compose-dev.yml

--- app/etc/env.prod.php
+++ app/etc/env.prod.php
@@ -91,8 +91,8 @@ return [
                     'base_link_url' => '{{secure_base_url}}',
                     'base_static_url' => 'https://rostyslav-panteleichuk.local/static/',
                     'base_media_url' => 'https://rostyslav-panteleichuk.local/media/'
-                ],
-            ],
+                ]
+            ]
         ],
         'websites' => [
             'additional_website' => [
