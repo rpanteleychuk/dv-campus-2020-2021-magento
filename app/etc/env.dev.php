1df0fe3830891da9c42c410cf09adac459f0ea97
Rostyslav Panteleichuk <r.panteleychuk@gmail.com>
2020-10-31 13:58:58 +0200

    #4 Magento upgrade: updated composer and Magento files

--- app/etc/env.dev.php
+++ app/etc/env.dev.php
@@ -91,8 +91,8 @@ return [
                     'base_link_url' => '{{secure_base_url}}',
                     'base_static_url' => 'https://rostyslav-panteleichuk-dev.local/static/',
                     'base_media_url' => 'https://rostyslav-panteleichuk-dev.local/media/'
-                ],
-            ],
+                ]
+            ]
         ],
         'websites' => [
             'additional_website' => [
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-BRANCH='lesson-3-pipline-deployment'
+BRANCH='lesson-4-magento-upgrade-and-creating-theme'
 
 php bin/magento deploy:mode:set default
 git checkout $BRANCH
