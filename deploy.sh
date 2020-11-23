4d59ffee80c22cff5902c626d6ee4eaa7bbc829b
Rostyslav Panteleichuk <r.panteleychuk@gmail.com>
2020-11-23 21:05:13 +0200

    #7 Blocks and templates: added uk_UA locale for the main website

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ composer install
 rm -rf var/cache/* var/page_cache/* var/view_preprocessed/* pub/static/frontend/* pub/static/adminhtml/* pub/static/_cache/* pub/static/deployed_version.txt generated/code/* generated/metadata/*
 php bin/magento setup:upgrade
 php bin/magento setup:di:compile
+php bin/magento setup:static-content:deploy uk_UA -f -a frontend --theme PRostik/luma --jobs=4
 php bin/magento setup:static-content:deploy en_US -f -a frontend --theme Magento/luma --jobs=4
-php bin/magento setup:static-content:deploy en_US -f -a frontend --theme PRostik/luma --jobs=4
 php bin/magento setup:static-content:deploy en_US -f -a adminhtml --jobs=4
 php bin/magento deploy:mode:set production --skip-compilation
--- dev/tools/grunt/configs/local-themes.js
+++ dev/tools/grunt/configs/local-themes.js
@@ -1,9 +1,20 @@
 /**
  * grunt clean:prostik_luma_en_us && grunt exec:prostik_luma_en_us && grunt less:prostik_luma_en_us && grunt watch
  * grunt exec:prostik_luma_en_us && grunt less:prostik_luma_en_us && grunt watch
+ * grunt exec:prostik_luma_uk_ua && grunt less:prostik_luma_uk_ua && grunt watch
  */
 
 module.exports = {
+    prostik_luma_uk_ua: {
+        area: 'frontend',
+        name: 'PRostik/luma',
+        locale: 'uk_UA',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
     prostik_luma_en_us: {
         area: 'frontend',
         name: 'PRostik/luma',
