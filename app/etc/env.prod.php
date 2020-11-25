00dbef125ad0c974787fb361b8652e31e19e7029
Rostyslav Panteleichuk <r.panteleychuk@gmail.com>
2020-11-25 19:39:23 +0200

    #7 Blocks and templates: edit Locale configuration for prod version

--- app/etc/env.prod.php
+++ app/etc/env.prod.php
@@ -95,6 +95,13 @@ return [
             ]
         ],
         'websites' => [
+            'base' => [
+                'general' => [
+                    'locale' => [
+                        'code' => 'uk_UA'
+                    ]
+                ]
+            ],
             'additional_website' => [
                 'web' => [
                     'unsecure' => [
